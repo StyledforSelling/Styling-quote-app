aa8627d2ff05f4ba8f9ab27f64368b7d381cd1bf
StyledforSelling <jeananne@styledforselling.com.au>
2024-11-06 23:30:13 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+<antArtifact identifier="tailwind-config" type="application/vnd.ant.code" language="javascript" title="Tailwind Config">
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+</antArtifact>
